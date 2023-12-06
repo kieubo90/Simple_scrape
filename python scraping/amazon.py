c8fd6790e29a117ae986d2e9d90d652e00a90395
Vu Kieu Phong Vu <vuvu.connect@gmail.com>
2023-12-06 16:06:12 +0200

    Add files via upload

---
+++ python scraping/amazon.py
@@ -0,0 +1,58 @@
+import requests
+custom_headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'accept-language': 'en-GB,en;q=0.9',
+}
+url = "https://www.amazon.de/s?k=mechanische+tastatur&crid=3B6JVNPU954TI&sprefix=mecha%2Caps%2C93&ref=nb_sb_ss_ts-doa-p_1_5"
+response = requests.get(url, headers= custom_headers)
+print(response.text)
+
+
+Most common class:
+Tag: button, class: sc-b035052a-0 FVrFk - Count: 48
+Tag: div, class: sc-7a37fa15-1 jszohx - Count: 24
+Tag: article, class: sc-76f76d49-0 fylEfR - Count: 24
+Tag: div, class: sc-6c8cecd9-2 eAuPiw - Count: 24
+Tag: a, class: sc-382aca2-0 jCzGWd - Count: 24
+Tag: img, class: sc-6c8cecd9-1 cLDhse sc-6c8cecd9-0 cmkywS - Count: 24
+Tag: div, class: sc-ddb02d9f-0 dHHZyS - Count: 24
+Tag: a, class: sc-382aca2-0 jCzGWd sc-382aca2-0 jCzGWd - Count: 24
+Tag: span, class: sc-4dcde147-0 egffZb - Count: 24
+Tag: div, class: sc-433536cb-0 bUDuaA - Count: 24
+Tag: div, class: sc-433536cb-2 ccdpCh - Count: 24
+Tag: div, class: sc-433536cb-1 iANWQd - Count: 24
+Tag: span, class: sc-68088102-0 jusdFT - Count: 24
+Tag: div, class: sc-67cf5218-0 WjVqn - Count: 24
+Tag: button, class: Buttonstyles__StyledButton-sc-1iu5c1d-1 dTA-dRc sc-dhKdcB bOsUiC sok-btn sok-btn-text-regular sok-btn-middle sok-btn-icon-only status-none theme-neutral block-none - Count: 24
+Tag: span, class: sc-26273634-0 gmBphi - Count: 24
+Tag: div, class: sc-6add5d36-0 jOndwT - Count: 24
+Tag: span, class: sc-1f6e57a5-0 eihZXX button-icon - Count: 24
+Tag: div, class: sc-6add5d36-1 ePzEgt - Count: 24
+Tag: input, class: sc-f15d1457-0 dyOVOY - Count: 24
+Tag: div, class: sc-fd4f6ed-0 fsxmRX - Count: 24
+Tag: span, class: sc-a63562c-0 koKbkz button-icon - Count: 24
+
+Most common class:
+Tag: div, class: sc-27ee5baf-3 hcYsmu - Count: 44
+Tag: button, class: sc-b035052a-0 FVrFk - Count: 38
+Tag: div, class: sc-7a37fa15-1 jszohx - Count: 19
+Tag: article, class: sc-76f76d49-0 fylEfR - Count: 19
+Tag: div, class: sc-6c8cecd9-2 eAuPiw - Count: 19
+Tag: a, class: sc-382aca2-0 jCzGWd - Count: 19
+Tag: img, class: sc-6c8cecd9-1 cLDhse sc-6c8cecd9-0 cmkywS - Count: 19
+Tag: div, class: sc-ddb02d9f-0 dHHZyS - Count: 19
+Tag: a, class: sc-382aca2-0 jCzGWd sc-382aca2-0 jCzGWd - Count: 19
+Tag: span, class: sc-4dcde147-0 egffZb - Count: 19
+Tag: div, class: sc-433536cb-0 bUDuaA - Count: 19
+Tag: div, class: sc-433536cb-2 ccdpCh - Count: 19
+Tag: div, class: sc-433536cb-1 iANWQd - Count: 19
+Tag: span, class: sc-68088102-0 jusdFT - Count: 19
+Tag: div, class: sc-67cf5218-0 WjVqn - Count: 19
+Tag: button, class: Buttonstyles__StyledButton-sc-1iu5c1d-1 dTA-dRc sc-dhKdcB bOsUiC sok-btn sok-btn-text-regular sok-btn-middle sok-btn-icon-only status-none theme-neutral block-none - Count: 19
+Tag: span, class: sc-26273634-0 gmBphi - Count: 19
+Tag: div, class: sc-6add5d36-0 jOndwT - Count: 19
+Tag: span, class: sc-1f6e57a5-0 eihZXX button-icon - Count: 19
+Tag: div, class: sc-6add5d36-1 ePzEgt - Count: 19
+Tag: input, class: sc-f15d1457-0 dyOVOY - Count: 19
+Tag: div, class: sc-fd4f6ed-0 fsxmRX - Count: 19
+Tag: span, class: sc-a63562c-0 koKbkz button-icon - Count: 19
